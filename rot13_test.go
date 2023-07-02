0abdde5bf389607f733107521a56a8726e51e6b6
Jakub Jarosz <jakub@jarosz.dev>
2023-07-02 07:45:42 +0100

    Attempt to connect to the server
    Illustrate that code compiles, server starts but attempt to connect to the server results in infinite loop. We try to abtain connection, wait for 10ms and try again.

--- rot13_test.go
+++ rot13_test.go
@@ -1,7 +1,10 @@
 package rot13_test
 
 import (
+	"fmt"
+	"net"
 	"testing"
+	"time"
 
 	"github.com/qba73/rot13"
 )
@@ -10,7 +13,27 @@ func TestServerSendsCorrectData(t *testing.T) {
 	t.Parallel()
 
 	// Start rot13 server
-
 	go rot13.RunServer()
 
+	// Connect to the server
+	conn := waitForConn()
+
+	// Send data to the connection
+	fmt.Fprintln(conn, "hello")
+}
+
+// waitForConn returns connection to the server.
+//
+// If the server is not ready yet to accept connections,
+// it waits 10ms before trying to connect again. As we launch
+// the server in a separate goroutine, waiting
+// until the server is ready to accept connections is necessary.
+func waitForConn() net.Conn {
+	for {
+		conn, err := net.Dial("tcp", ":8080")
+		if err == nil {
+			return conn
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
 }
